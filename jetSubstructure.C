c107a4a441279f030809273540da8f4d29cc11e1
chihyeh <31882589+chihyeh@users.noreply.github.com>
2017-11-19 10:00:07 +0800

    Add files via upload

--- jetSubstructure.C
+++ jetSubstructure.C
@@ -31,50 +31,47 @@ float myDeltaR(float eta1, float eta2, float phi1, float phi2)
 }
 
 
-void jetSubstructure(string inputDir, float radius=0.4, int mode=0, float energy=0.5){
+void jetSubstructure(string inputDir, float radius=0.4, int mode=0){
 
   string decversion;
   if(inputDir.find("rfull009")!=std::string::npos)decversion="rfull009";
   else if(inputDir.find("rfull012")!=std::string::npos)decversion="rfull012";
   else if(inputDir.find("rfull010")!=std::string::npos)decversion="rfull010";
 
-  std::string treeName = "trawhits";
+  std::string treeName = "tGEN_nonu";
   std::string title = Form("Anti-kt jet #Delta R = %.1f",radius);
 
 
 
   const std::string histonames[]={
-//     "mass_trim",
-//     "mass_mmdt",
-//     "mass_prun",
-//     "mass_sdb2",
-//     "mass_sdm1",
-    "tau21_b1",
-    "tau32_b1",
-    "c2_b1"//,
-//     "d2_b1",
-//     "n2_b1",
-//     "tau21_b1_mmdt",
-//     "c2_b1_mmdt",
-//     "d2_b1_mmdt",
-//     "n2_b1_mmdt"
+     "mass_trim",
+     "mass_mmdt",
+     "mass_prun",
+     "mass_sdb2",
+     "mass_sdm1",
+     "tau21_b1",
+     "tau32_b1",
+     "c2_b1",
+     "d2_b1",
+     "n2_b1",
+     "tau21_b1_mmdt",
+     "c2_b1_mmdt",
+     "d2_b1_mmdt",
+     "n2_b1_mmdt"
   };
 
 
-    int nTotal=0;
-    int nPass=0;
-    int k;
-    int j;
+
   //  int ntemp_histos=sizeof(histonames)/sizeof(histonames[0]);
-  const int nhistos=3;//ntemp_histos;
+  const int nhistos=14;//ntemp_histos;
 
   std::cout << "Total number of histograms is " << nhistos << std::endl;
-  //  const float xmax[nhistos]={400,400,400,400,400,1,0.3,20,1,1,0.3,20,1};
-  const float xmax[nhistos]={1,1,0.3};
+  const float xmax[nhistos]={400,250,400,400,400,1,0.3,20,1,1,0.3,20,1,1};
+  //const float xmax[nhistos]={1,1,0.3};
 
   TH1F* h_sub[nhistos];
   const float xmin=0;
-  const int nbins=100;
+  const int nbins=25;
   for(int ih=0; ih < nhistos; ih++)
     {
       h_sub[ih] = new TH1F(Form("h_%s",histonames[ih].data()), histonames[ih].data(), nbins,xmin,xmax[ih]);
@@ -83,7 +80,7 @@ void jetSubstructure(string inputDir, float radius=0.4, int mode=0, float energy
       h_sub[ih]->SetYTitle(Form("Number of jets per %.2f",binwidth));
     }
 
-  string inputFile = inputDir + "/radius" + Form("%0.1f",radius)+ "_rawhit_fastjet_mode" + Form("%d",mode) + "_"+Form("%.1f",energy)+"GeV.root"; //"_response_e2.root";
+  string inputFile = inputDir + "/radius" + Form("%0.1f",radius)+ "_response_tGEN_nonu2.root";
   cout << "opening " << inputFile.data() << endl;
   TreeReader genTree(inputFile.data(),"tGEN_nonu");
   TreeReader caloTree(inputFile.data(),treeName.data());
@@ -103,16 +100,16 @@ void jetSubstructure(string inputDir, float radius=0.4, int mode=0, float energy
     Float_t*  calo_jeta = caloTree.GetPtrFloat("jeta");
     Float_t*  calo_jphi = caloTree.GetPtrFloat("jphi");
     Int_t     calo_njets    = caloTree.GetInt("njets");
-    Float_t*  tau21     = caloTree.GetPtrFloat("j_tau21_b1");
 
     Float_t*  sub[nhistos];
-    
-     
+
     for(int ih=0; ih < nhistos; ih++)
-    sub[ih] = caloTree.GetPtrFloat(Form("j_%s",histonames[ih].data()));
-    
-      for(int i=0; i< calo_njets; i++){
- 
+      sub[ih] = caloTree.GetPtrFloat(Form("j_%s",histonames[ih].data()));
+
+
+    for(int i=0; i< calo_njets; i++){
+
+      
       if(fabs(calo_jeta[i])>1.1)continue;
 
       int findGenMatch=-1;
@@ -121,7 +118,7 @@ void jetSubstructure(string inputDir, float radius=0.4, int mode=0, float energy
 	
  	float dr = myDeltaR(gen_jeta[k], calo_jeta[i],
 			    gen_jphi[k], calo_jphi[i]);
-         
+
  	if(dr<0.1)
  	  {
  	    findGenMatch=k;
@@ -131,21 +128,16 @@ void jetSubstructure(string inputDir, float radius=0.4, int mode=0, float energy
 
       if(findGenMatch<0)continue;
 
-    for(int ih=0; ih < nhistos; ih++)
-        h_sub[ih]->Fill(sub[ih][i]);
-        nTotal++;
-          
-        if(tau21[i]< 0.9)
-        nPass++;
-          
-        
-    } // end of loop over calo jets
-    cout<<"efficiency="<<nPass/nTotal<<endl;
-  } // end loop of entries
-  
-    
+      for(int ih=0; ih < nhistos; ih++)    
+	h_sub[ih]->Fill(sub[ih][i]);
 
-  string outputFile = inputDir + "/radius" + Form("%0.1f",radius)+ "_jetsubstructure_mode"+Form("%d",mode)+"_"+treeName + "_"+Form("%.1f",energy)+"GeV.root";
+
+    } // end of loop over calo jets
+  } // end loop of entries
+     
+  
+
+  string outputFile = inputDir + "/radius" + Form("%0.1f",radius)+ "_jetsubstructure_" + treeName + ".root";
   cout << "writing output to " << outputFile.data() << endl;
 
   TFile* outFile = new TFile(outputFile.data(),"recreate");
